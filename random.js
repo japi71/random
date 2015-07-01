5c04051c082c6840d83a6857fa388c7bd3c960b9
javi <Javi>
2015-07-01 13:35:08 +0200

    esto es un comentario

---
+++ random.js
@@ -0,0 +1,13 @@
+//-----------------------------------
+//Autor: Antonio Javier Laguna López
+//-----------------------------------
+//Proyecto GitHub - Random
+//-----------------------------------
+
+var numero = Math.random();
+
+if(numero <= 0.5){
+    console.log('\n' + numero + ' Menor que 0,5\n');
+}else{
+    console.log('\n' + numero + ' Mayorque 0,5\n');
+}
